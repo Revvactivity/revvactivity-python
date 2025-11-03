8e220fb4ad51355694d8a49d17f60804c09527e6
Tom <tom.weelborg@gmail.com>
2025-11-03 19:17:52 +0100

    feat: add Signal update listening support

--- src/revvactivity_python/signal/signals/signal.py
+++ src/revvactivity_python/signal/signals/signal.py
@@ -1,4 +1,8 @@
+from inspect import signature
 from typing import Generic, TypeVar
+
+from revvactivity_python.signal.listeners.signal_change_listener import SignalChangeListener
+from revvactivity_python.signal.listeners.signal_update_listener import SignalUpdateListener
 
 
 T = TypeVar("T")
@@ -9,9 +13,27 @@ class Signal(Generic[T]):
     def __init__(self, value: T | None = None):
         super().__init__()
         self.__value: T | None = value
+        self.__update_listeners: list[SignalChangeListener[T_super]] = []
     
     def get_value(self) -> T | None:
         return self.__value
     
     def _set_value(self, value: T | None) -> None:
+        old_value = self.__value
         self.__value = value
+
+        self._notify_update_listeners(old_value)
+
+    def _notify_update_listeners(self, old_value: T | None) -> None:
+        for update_listener in self.__update_listeners:
+            update_listener(old_value, self.__value)
+    
+    def on_update(self, listener: SignalUpdateListener[T_super] | SignalChangeListener[T_super]) -> None:
+        self.__on_helper(listener, self.__update_listeners)
+    
+    def __on_helper(self, listener: SignalUpdateListener[T_super] | SignalChangeListener[T_super], list_of_listeners: list[SignalChangeListener[T_super]]) -> None:
+        sig = signature(listener)
+        if len(sig.parameters) == 1:
+            original_listener = listener
+            listener = lambda old_value, new_value: original_listener(new_value)
+        list_of_listeners.append(listener)
