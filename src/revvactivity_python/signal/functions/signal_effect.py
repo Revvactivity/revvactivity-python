69fa6d5ed6e4e600eb02647f4cf5d10cc106e664
Tom <tom.weelborg@gmail.com>
2025-11-03 19:22:09 +0100

    feat: add SignalEffect

---
+++ src/revvactivity_python/signal/functions/signal_effect.py
@@ -0,0 +1,11 @@
+from typing import Protocol
+
+from revvactivity_python.signal.functions.signal_function import SignalFunction
+
+
+class SignalEffect(SignalFunction, Protocol):
+    def __call__(self) -> None:
+        ...
+    
+    def execute(self) -> None:
+        return self()
