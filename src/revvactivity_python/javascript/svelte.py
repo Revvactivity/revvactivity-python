7f08f509e1507206ba09d93c2c3cf925925d1c8b
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-03 19:24:28 +0100

    feat: add Svelte-like reactivity functions

---
+++ src/revvactivity_python/javascript/svelte.py
@@ -0,0 +1,26 @@
+from typing import TypeVar
+
+from revvactivity_python.javascript.javascript_framework import JavaScriptFramework
+from revvactivity_python.signal.functions.signal_derived import SignalDerived
+from revvactivity_python.signal.functions.signal_effect import SignalEffect
+from revvactivity_python.signal.signals.signal import Signal
+from revvactivity_python.signal.signals.write_signal import WriteSignal
+
+
+T = TypeVar("T")
+
+class Svelte(JavaScriptFramework):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def state(value: T) -> WriteSignal[T]:
+        return JavaScriptFramework._state(value)
+    
+    @staticmethod
+    def derived(derived: SignalDerived[T]) -> Signal[T]:
+        return JavaScriptFramework._derived(derived)
+    
+    @staticmethod
+    def effect(effect: SignalEffect) -> None:
+        JavaScriptFramework._effect(effect)
