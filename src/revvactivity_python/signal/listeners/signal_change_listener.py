20b741a0e1a9bc7eff7a1426865fa28d3ac68d2e
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-03 19:14:32 +0100

    feat: add SignalChangeListener

---
+++ src/revvactivity_python/signal/listeners/signal_change_listener.py
@@ -0,0 +1,13 @@
+from typing import Protocol, TypeVar
+
+from revvactivity_python.signal.listeners.signal_listener import SignalListener
+
+
+T = TypeVar("T")
+
+class SignalChangeListener(SignalListener[T], Protocol[T]):
+    def __call__(self, old_value: T, new_value: T) -> None:
+        ...
+    
+    def on_change(self, old_value: T, new_value: T) -> None:
+        return self()
