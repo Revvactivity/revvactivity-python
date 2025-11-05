1a59450274448d1a3134a2c47a205426ce60eca2
Tom <tom.weelborg@gmail.com>
2025-11-06 00:32:19 +0100

    fix: SignalFunctionWrapper __get_signals

--- src/revvactivity_python/signal/functions/signal_function_wrapper.py
+++ src/revvactivity_python/signal/functions/signal_function_wrapper.py
@@ -8,12 +8,15 @@ from revvactivity_python.signal.signals.signal import Signal
 class SignalFunctionWrapper:
     def on_update_signals(self, signal_function: SignalFunction, update_listener: SignalUpdateListener[Any]) -> None:
         signals = self.__get_signals(signal_function)
-        for signal in signals.values():
+        for signal in signals:
             signal.on_update(update_listener)
     
-    def __get_signals(self, signal_function: SignalFunction) -> dict[str, Signal[Any]]:
-        return {
-            name: signal_function.__globals__[name]
+    def __get_signals(self, signal_function: SignalFunction) -> list[Signal[Any]]:
+        globals = [
+            signal_function.__globals__[name]
             for name in signal_function.__code__.co_names
             if name in signal_function.__globals__
-        }
+        ]
+        closures = [cell.cell_contents for cell in signal_function.__closure__] if signal_function.__closure__ else []
+        all_vars = globals + closures
+        return [item for item in all_vars if isinstance(item, Signal)]
