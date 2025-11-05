38bf739a6b4db0fdaea332164d460b2db0ad8af2
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-06 00:45:44 +0100

    test: add SvelteTest

---
+++ tests/revvactivity_python/javascript/svelte.py
@@ -0,0 +1,25 @@
+from typing import Callable, Type, TypeVar
+
+from revvactivity_python.javascript.javascript_framework import JavaScriptFramework
+from revvactivity_python.javascript.svelte import Svelte
+from revvactivity_python.signal.functions.signal_derived import SignalDerived
+from revvactivity_python.signal.functions.signal_effect import SignalEffect
+from revvactivity_python.signal.signals.signal import Signal
+from revvactivity_python.signal.signals.write_signal import WriteSignal
+from tests.revvactivity_python.javascript.javascript_framework import JavaScriptFrameworkTest
+
+
+T = TypeVar("T")
+
+class SvelteTest(JavaScriptFrameworkTest):
+    def _get_type(self) -> Type[JavaScriptFramework]:
+        return Svelte
+
+    def _get_state_function(self) -> Callable[[T], WriteSignal[T]]:
+        return Svelte.use_state
+
+    def _get_derived_function(self) -> Callable[[SignalDerived[T]], Signal[T]]:
+        return Svelte.use_memo
+
+    def _get_effect_function(self) -> Callable[[SignalEffect], None]:
+        return Svelte.use_effect
